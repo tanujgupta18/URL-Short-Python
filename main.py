b2fabe08dd2c8bb42f6ca4ea60778c9ae6194e67
Tanuj Gupta <64638288+tanujgupta18@users.noreply.github.com>
2023-12-15 00:30:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-import streamlit as st
 import pyshorteners
+
+url = input("Enter the URL: ")
 
 def short_url(url):
     s = pyshorteners.Shortener()
-    return s.tinyurl.short(url)
+    print(s.tinyurl.short(url))
 
-st.title("URL Shortener App")
-
-url = st.text_input("Enter the URL:")
-if st.button("Shorten URL"):
-    if url:
-        short_url_result = short_url(url)
-        st.success(f"Shortened URL: {short_url_result}")
-    else:
-        st.warning("Please enter a URL")
+short_url(url)
